01335f735c7e163856f71fef17167f8b5346dfb5
Surface <petergrant@uvic.ca>
2023-11-30 20:22:14 -0800

    Showing how it works

--- CAN Test Bench/Core/Inc/dash.h
+++ CAN Test Bench/Core/Inc/dash.h
@@ -2,5 +2,27 @@
 
 // This file will define message data as human readable stuff
 
+// The dash does not have a unique CAN ID
+// Each function on the dash has a unique CAN ID
 
 #include "main.h"
+
+
+// CAN IDs
+
+// Battery Temperature 0x82
+
+// Motor Temperature 0x87
+
+// Inverter Temperature TODO
+
+// RPM 0x80
+
+// State of Charge 0x87
+
+// Need to define these
+
+
+
+
+
