69453a57cb7f4c5780808ef8e74bfb4677359579
ethan <ethan@DESKTOP-NMNDMNE>
2023-12-09 17:41:49 -0800

    Added constants for pdu CAN messages in pdu.h and created enable and disable functions in pdu.c

--- CAN Test Bench/Core/Inc/pdu.h
+++ CAN Test Bench/Core/Inc/pdu.h
@@ -8,22 +8,38 @@
 // Can ID: 0x710
 
 enum pdu_messages_5A{
-	enable_speaker = 0x1C,
-	disable_speaker = 0x0C,
-	enable_brake_light = 0x1B,
-	disable_brake_light = 0x0B,
-	enable_motor_controller = 0x1E,
-	disable_motor_controller = 0x0E,
-	enable_shutedown_circuit = 0x1F,
-	disable_shutedown_circuit = 0x0F
+	enable_speaker_msg = 0x1C,
+	disable_speaker_msg = 0x0C,
+	enable_brake_light_msg = 0x1B,
+	disable_brake_light_msg = 0x0B,
+	enable_motor_controller_msg = 0x1E,
+	disable_motor_controller_msg = 0x0E,
+	enable_shutdown_circuit_msg = 0x1F,
+	disable_shutdown_circuit_msg = 0x0F
 };
 
 enum pdu_messages_20A{
-	enable_cooling_fan1 = 0x33,
-	disable_cooling_fan1 = 023,
-	enable_cooling_fan2 = 0x34,
-	disable_cooling_fan2 = 0x24,
-	enable_coolant_pump = 0x31,
-	disable_coolant_pump = 0x21
-
+	enable_left_cooling_fan_msg = 0x33,
+	disable_left_cooling_fan_msg = 0x23,
+	enable_right_cooling_fan_msg = 0x34,
+	disable_right_cooling_fan_msg = 0x24,
+	enable_coolant_pump_msg = 0x31,
+	disable_coolant_pump_msg = 0x21
 };
+
+
+// PDU functions (5 Amp)
+void speaker_chirp();
+void toggle_brake_light();
+void disable_brake_light();
+void enable_motor_controller();
+void disable_motor_controller();
+void enable_shutdown_circuit();
+void disable_shutdown_circuit();
+
+// PDU functions (20 Amp)
+void enable_cooling_fans();
+void disable_cooling_fans();
+void enable_coolant_pump();
+void disable_coolant_pump();
+
--- CAN Test Bench/Core/Src/pdu.c
+++ CAN Test Bench/Core/Src/pdu.c
@@ -4,3 +4,190 @@
 #include "pdu.h"
 #include "can.h"
 #include "main.h"
+
+
+// PDU commands for 5A Circuit
+void speaker_chirp(){
+/* Chirp to indicate vehicle ready to drive*/
+    TxHeader.StdId=0x710; // This is the CAN ID
+    TxHeader.DLC=1; // Data Length Code
+
+    TxData[0] = enable_speaker_msg;
+
+    if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+    {
+		 /* Transmission request Error */
+		 Error_Handler();
+    }
+    //TODO: is this the correct delay?
+	HAL_Delay(1000);
+	TxData[0] = disable_speaker_msg;
+	if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	{
+		/* Transmission request Error */
+		Error_Handler();
+	}
+}
+
+void toggle_brake_light(){
+
+	TxHeader.StdId=0x710;
+    TxHeader.DLC=1; // Data Length Code
+
+    TxData[0] = enable_brake_light_msg;
+
+	 if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	 {
+		 /* Transmission request Error */
+		 Error_Handler();
+	 }
+}
+
+void enable_brake_light(){
+
+	TxHeader.StdId=0x710; // PDU CAN ID
+    TxHeader.DLC=1; // Data Length Code
+
+    TxData[0] = enable_brake_light_msg;
+
+	 if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	 {
+		 /* Transmission request Error */
+		 Error_Handler();
+	 }
+}
+
+void disable_brake_light(){
+
+	TxHeader.StdId=0x710; // PDU CAN ID
+    TxHeader.DLC=1; // Data Length Code
+
+    TxData[0] = disable_brake_light_msg;
+
+	 if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	 {
+		 /* Transmission request Error */
+		 Error_Handler();
+	 }
+}
+
+void enable_motor_controller(){
+	TxHeader.StdId=0x710; // PDU CAN ID
+	TxHeader.DLC=1; // Data Length Code
+
+	TxData[0] = enable_motor_controller_msg;
+
+	if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	{
+		/* Transmission request Error */
+		Error_Handler();
+	}
+}
+void disable_motor_controller(){
+	TxHeader.StdId=0x710; // PDU CAN ID
+	TxHeader.DLC=1; // Data Length Code
+
+	TxData[0] = disable_motor_controller_msg;
+
+	if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	{
+		/* Transmission request Error */
+		Error_Handler();
+	}
+}
+
+void enable_shutdown_circuit(){
+	TxHeader.StdId=0x710; // PDU CAN ID
+	TxHeader.DLC=1; // Data Length Code
+
+	TxData[0] = enable_shutdown_circuit_msg;
+
+	if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	{
+		/* Transmission request Error */
+		Error_Handler();
+	}
+}
+
+void disable_shutdown_circuit(){
+	TxHeader.StdId=0x710; // PDU CAN ID
+	TxHeader.DLC=1; // Data Length Code
+
+	TxData[0] = disable_shutdown_circuit_msg;
+
+	if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	{
+		/* Transmission request Error */
+		Error_Handler();
+	}
+}
+
+
+// PDU commands for 20A Circuit
+void enable_cooling_fans(){
+	TxHeader.StdId=0x710; // PDU CAN ID
+	TxHeader.DLC=1; // Data Length Code
+
+	TxData[0] = enable_left_cooling_fan_msg;
+
+	if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	{
+		/* Transmission request Error */
+		Error_Handler();
+	}
+
+	TxData[0] = enable_right_cooling_fan_msg;
+
+	if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	{
+		/* Transmission request Error */
+		Error_Handler();
+	}
+}
+
+void disable_cooling_fans(){
+	TxHeader.StdId=0x710; // PDU CAN ID
+	TxHeader.DLC=1; // Data Length Code
+
+	TxData[0] = disable_left_cooling_fan_msg;
+
+	if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	{
+		/* Transmission request Error */
+		Error_Handler();
+	}
+
+	TxData[0] = disable_right_cooling_fan_msg;
+
+	if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	{
+		/* Transmission request Error */
+		Error_Handler();
+	}
+}
+
+
+void enable_coolant_pump(){
+	TxHeader.StdId=0x710; // PDU CAN ID
+	TxHeader.DLC=1; // Data Length Code
+
+	TxData[0] = enable_coolant_pump_msg;
+
+	if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	{
+		/* Transmission request Error */
+		Error_Handler();
+	}
+}
+void disable_coolant_pump(){
+	TxHeader.StdId=0x710; // PDU CAN ID
+	TxHeader.DLC=1; // Data Length Code
+
+	TxData[0] = disable_coolant_pump_msg;
+
+	if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	{
+		/* Transmission request Error */
+		Error_Handler();
+	}
+}
