2e3ce698fba986bb460f40966bf7e3b4bc4af80a
petergrant13 <96028271+petergrant13@users.noreply.github.com>
2023-12-18 14:46:34 -0800

    Add some functions for IMD, upgraded SW version

--- CAN Test Bench/Core/Inc/imd.h
+++ CAN Test Bench/Core/Inc/imd.h
@@ -2,9 +2,116 @@
 
 // This file will define message data as human readable stuff
 
-
 #include "main.h"
 
 
-// CAN ID is extended
-// ID=0x0A100100
+// CAN ID is currently extended
+
+// Needs to be changed to standard ID
+
+// Faults
+// The first byte returned from a status request message will have 8 status bits and the data as the rest
+enum status_bits{
+	// I have just left this as plain binary. Could also be declared by bitshifting but I think this is easier
+	//	    					12345678
+	Isolation_status_bit0	= 0b00000001, // IS0
+	Isolation_status_bit1	= 0b00000010, // IS1
+	Low_Battery_Voltage		= 0b00000100, // LV 15V threshold (1 if below 15V)
+	High_Battery_Voltage	= 0b00001000, // HV (1 if higher than Max_battery_working_voltage)
+	Exc_off 				= 0b00010000, // EO (Excitation pulse 0 for operating)
+	High_Uncertainty 		= 0b00100000, // HU (1 for greater than 5%)
+	Touch_energy_fault 		= 0b01000000, // EF (1 for energy exceeds 0.2J)
+	Hardware_Error			= 0b10000000, // HE (1 for error)
+};
+
+
+// The MCU needs to send a message to the IMD requesting info
+// These are requests that will return status bits (defined above) & the value requested
+enum status_requests{
+	// The electrical isolation is in bytes 2 & 3
+	isolation_state = 0xE0,
+
+	// Rp is the resistance from the positive of the battery to chassis
+	// Rn is the resistance from the negative of the battery to chassis
+	isolation_resistances = 0xE1,
+
+	// Cp is the capacitance from the positive of the battery to chassis
+	// Cn is the capacitance from the negative of the battery to chassis
+	// Bytes 2 & 3 are Cp, bytes 5 & 6 are Cn, in nF
+	isolation_capacitances = 0xE2,
+
+	// High voltage battery voltages to chassis
+	voltages_Vp_and_Vn = 0xE3,
+
+	// GLV battery voltage
+	battery_voltage = 0xE4,
+
+	// Error flags are a series of bits described below
+	Error_flags = 0xE5,
+
+	// The IMD monitors charge stored in the system
+	safety_touch_energy = 0xE6,
+
+	// Also monitors if it is safe to touch
+	safety_touch_current = 0xE7,
+
+	// This is just a parameter we can set
+	Max_battery_working_voltage = 0xF0,
+
+	// We can read the temperature of the board
+	Temperature = 0x80,
+};
+
+// If one of the error flags is set, then the harware error bit will go to 1
+enum error_flags{
+	// Bits 0-6 are reserved
+	Err_temp = 0x0080, // HT high temperature (0 if temperature is below 105C)
+	Err_clock = 0x0100, // CE  (0 if clock is good)
+	Err_Watchdog = 0x0200, // WD Watchdog (0 if watchdog is good)
+	Err_Vpwr = 0x0400, // V_PWR power supply (0 if power supply voltage is good)
+	Err_Vexi = 0x0800, // V_EXI Excitation voltage (0 if excitation voltage is good)
+	Err_VxR = 0x1000, // VxR (0 if both Vx1 and Vx2 are good)
+	Err_CH = 0x2000, // CH chassis connections (0 if both chassis connections are good)
+	Err_Vx1 = 0x4000, // Vx1 connection (0 if SIM101 connection to Hv+ is good)
+	Err_Vx2 = 0x8000, // Vx2 connection (0 if SIM101 connection to Hv- is good)
+};
+
+// Probably not useful and won't be used for a bit but may be useful in the future
+enum manufacturer_requests{
+	// The numbers get broken up over multiple bytes
+	Part_name_0 = 0x01,
+	Part_name_1 = 0x02,
+	Part_name_2 = 0x03,
+	Part_name_3 = 0x04,
+	Version_0 = 0x05,
+	Version_1 = 0x06,
+	Version_2 = 0x07,
+	Serial_number_0 = 0x08,
+	Serial_number_1 = 0x09,
+	Serial_number_2 = 0x0A,
+	Serial_number_3 = 0x0B,
+	Uptime_counter = 0x0C,
+	Vn_hi_res = 0x60,
+	Vp_hi_res = 0x61,
+	Vexc_hi_res = 0x62,
+	Vb_hi_res = 0x63,
+	Vpwr_hi_res = 0x65,
+};
+
+
+// Function declarations
+void IMD_Parse_Message(int DLC, int Data[8]);
+
+
+// Functions to check states are okay
+void Check_Isolation_State(int Data[8]);
+
+// Functions to read data from the IMD
+void Request_Isolation_State(void);
+void Request_Isolation_Resistances(void);
+
+
+
+
+
+
--- CAN Test Bench/Core/Src/imd.c
+++ CAN Test Bench/Core/Src/imd.c
@@ -1,4 +1,151 @@
 // This where the code to handle IMD errors and such will go
 
 
-#include "IMD.h"
+#include "imd.h"
+
+// We need to include can.h because we will send CAN messages through the functions in that file
+// When a CAN message comes it will throw an interrupt can.c deals with the incoming message
+// the function in can.c gets the ID and sends the data to  the functions here
+#include "can.h"
+#include "main.h"
+#include "constants.h"
+
+// We need to include pdu.h for the shutdown circuit
+#include "pdu.h"
+
+
+int IMD_CAN_ID = 0x23; // This is the ID for MCU sending msg to IMD
+// MCU will receive messages with id 0x24 from IMD
+
+uint8_t IMD_status_bits = 0;
+
+
+// Need a function to parse the CAN message data received from the IMD
+void IMD_Parse_Message(int DLC, int Data[8]){
+	// The first step is to look at the first byte to figure out what we're looking at
+	// Need to make sure that Data[0] really is the first byte
+	switch (Data[0]){
+		case isolation_state:
+			// do something
+		break;
+		case isolation_resistances:
+			// do something
+		break;
+		case isolation_capacitances:
+			// do something
+		break;
+		case voltages_Vp_and_Vn:
+			// do something
+		break;
+		case battery_voltage:
+			// do something
+		break;
+		case Error_flags:
+			// do something
+		break;
+		case safety_touch_energy:
+			// do something
+		break;
+		case safety_touch_current:
+			// do something
+		break;
+		case Max_battery_working_voltage:
+			// do something
+		break;
+		case Vn_hi_res:
+			// do something
+		break;
+		case Vp_hi_res:
+			// do something
+		break;
+		case Vexc_hi_res:
+			// do something
+		break;
+		case Vb_hi_res:
+			// do something
+		break;
+		case Vpwr_hi_res:
+			// do something
+		break;
+		case Temperature:
+			// do something
+		break;
+		default:
+			Error_Handler();
+		break;
+	}
+
+}
+
+// --------------------------------------------------------------------------------------
+// Functions to check status
+// AFAIK the IMD will not send a CAN msg when the status changes - we need to constantly poll it
+// --------------------------------------------------------------------------------------
+// This is the function that will be called when a CAN message is received that has the isolation state data
+void Check_Isolation_State(int Data[8]){
+	// The first byte is the status bits. If any are non-zero -> error -> stop power to shutdown circuit
+	if (Data[1] != 0){
+		disable_shutdown_circuit();
+	}
+
+	// Then check the rest of the message
+	int isolation = (Data[2] << 8) | Data[3];
+
+	if (isolation < 500){
+		disable_shutdown_circuit();
+	}
+
+	// Can check tolerances and such but are far less important
+	// debugging
+	HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
+}
+
+
+
+
+// --------------------------------------------------------------------------------------
+// Functions to send messages to the IMD (below)
+// These send the message to request data
+// The IMD will then send a message with the same code and the data. This is read in functions above
+// --------------------------------------------------------------------------------------
+void Request_Isolation_State(void){
+	TxHeader.StdId = IMD_CAN_ID;
+	TxHeader.DLC = 1;
+	TxData[0] = isolation_state;
+
+	if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK){
+		/* Transmission request Error */
+		Error_Handler();
+	  }
+}
+
+void Request_Isolation_Resistances(void){
+	TxHeader.StdId = IMD_CAN_ID;
+	TxHeader.DLC = 1;
+	TxData[0] = isolation_resistances;
+
+	if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK){
+		/* Transmission request Error */
+		Error_Handler();
+	  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --------------------------------------------------------------------------------------
+// Function to remove power to shutdown circuit
+
+
+
