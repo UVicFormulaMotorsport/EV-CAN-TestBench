11190d949c5d6c0c1533fc2d0796df944ab69711
bir0ni <byo1000@gmail.com>
2024-03-11 11:29:36 -0700

    The code is alive, please send help

--- CAN Test Bench/Core/Src/errors.c
+++ CAN Test Bench/Core/Src/errors.c
@@ -5,16 +5,50 @@
 
 extern error_struct _error_LUT[];
 
+uint32_t error_bitfield[8] = {0,0,0,0,0,0,0,0};
+
 
 void _throw_error(enum error_id id){
 	error_struct error = _error_LUT[id];
 	if(error.type_info & ERR_SHUTDOWN_MASK ){
-
+		_SHUTDOWN;
+	}else if(error.type_info & ERR_SUSPEND_MASK){
+		if(error.resume_condition != NULL){
+			_SUSPEND;
+		} else {
+			_SHUTDOWN;
+		}
 	}
 
-}
-void _throw_error_can(enum error_id id, char* can_msg){
+#if _ERR_DISPLAY_ENABLE
+	if((error.type_info & ERR_DISPLAY_MASK) && (error.msg != NULL)){
+		//Display error message to dash
+
+	}
+#endif
+
+	//logging
+#if _ERR_LOGGING_ENABLE
+	_log_error(e, NULL);
+#endif
+
 
 }
+
+void _throw_error_int32(enum error_id id, int32_t arg)
+{
+
+}
+
+void _throw_error_float(enum error_id id, float arg)
+{
+
+}
+
+#if _ERR_LOGGING_ENABLE
+	void _log_error(error_struct e, char* params){
+
+	}
+#endif
 
 
