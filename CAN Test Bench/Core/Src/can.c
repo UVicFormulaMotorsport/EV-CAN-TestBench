bbb54eb2a254ee64a08936d09b4fa3e743e34ca0
karni <karni@134.87.33.214>
2024-12-08 13:47:03 -0800

    motor controller things

--- CAN Test Bench/Core/Src/can.c
+++ CAN Test Bench/Core/Src/can.c
@@ -220,3 +220,25 @@ void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan2){
 }
 
 /* USER CODE END 1 */
+
+
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    can.c
+  * @brief   This file provides code for the configuration
+  *          of the CAN instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
