b7b4f911c816c9156aba8832a3d4e4037eda1c67
Rachan Grewal <rachanpreetgrewal@uvic.ca>
2025-02-12 23:19:33 -0800

    "driving_loop.c updated with more comments for software team"

--- CAN Test Bench/Core/Src/driving_loop.c
+++ CAN Test Bench/Core/Src/driving_loop.c
@@ -23,10 +23,10 @@ extern uint16_t adc1_BPS1;
 extern uint16_t adc1_BPS2;
 
 enum uv_status_t initDrivingLoop(void *argument){
-	uv_task_info* dl_task = uvCreateTask();
+	uv_task_info* dl_task = uvCreateTask(); // allocate memory for the task
 
 	if(dl_task == NULL){
-		//Oh dear lawd
+		//Oh dear lawd if allocation fails return error
 		return UV_ERROR;
 	}
 
@@ -37,25 +37,25 @@ enum uv_status_t initDrivingLoop(void *argument){
 	//if(dl_task->task_name == NULL){
 		//return UV_ERROR; //failed to malloc the name of the thing
 	//}
-	dl_task->task_name = "Driving_Loop";
+	dl_task->task_name = "Driving_Loop"; // assign name
 
 
-	dl_task->task_function = StartDrivingLoop;
-	dl_task->task_priority = osPriorityHigh;
+	dl_task->task_function = StartDrivingLoop; // defining the function the task will run
+	dl_task->task_priority = osPriorityHigh; // assigns a high priority in FreeRTOS
 
-	dl_task->stack_size = 256;
+	dl_task->stack_size = 256; // memory allocation for task execution
 
-	dl_task->active_states = UV_DRIVING;
+	dl_task->active_states = UV_DRIVING; // Specifies when the task should be active
 	dl_task->suspension_states = 0x00;
-	dl_task->deletion_states = UV_READY | PROGRAMMING | UV_LAUNCH_CONTROL | UV_ERROR_STATE;
+	dl_task->deletion_states = UV_READY | PROGRAMMING | UV_LAUNCH_CONTROL | UV_ERROR_STATE; // defines conditions where the task should be deleted.
 
-	dl_task->task_period = 100;//0.1 seconds
+	dl_task->task_period = 100;//runs every 100ms or 0.1 seconds
 
 	//
 
 	dl_task->task_args = NULL; //TODO: Add actual settings dipshit
 
-	return UV_OK;
+	return UV_OK; //
 }
 
 /**@brief  Function implementing the ledTask thread.
@@ -72,6 +72,8 @@ void StartDrivingLoop(void * argument){
 	 *	We will be caching variables that are used very frequently in every single loop iteration, and are not
 	 */
 
+
+	//extracting task arguments, and gets parameters like min/max allowed values for the APPS and BPS
 	uv_task_info* params = (uv_task_info*) argument;
 	/*
 	uint16_t min_apps_value;
@@ -98,37 +100,42 @@ void StartDrivingLoop(void * argument){
 
 	/**These here lines set the delay. This task executes exactly at the period specified, regardless of how long the task
 		 * execution actually takes
+		 * rachan: ensures the function runs exactly 100ms, regardless of execution time.
 		 *
 		 @code*/
 	TickType_t tick_period = pdMS_TO_TICKS(params->task_period); //Convert ms of period to the RTOS ticks
 	TickType_t last_time = xTaskGetTickCount();
 	/**@endcode */
-	for(;;){
+	for(;;){ // enters infinite loop
 
 
-		if(params->cmd_data == UV_KILL_CMD){
+		if(params->cmd_data == UV_KILL_CMD){ // to perform task control (suspend/kill)
 			killSelf(params);
 		}else if(params->cmd_data == UV_SUSPEND_CMD){
 			suspendSelf(params);
 		}
 		vTaskDelayUntil( &last_time, tick_period); //Me and the boys on our way to wait for a set period
 
-		HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
+		HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14); //Blink and LED (for debugging)
 		//Read stuff from the addresses
 
 		//Copy the values over into new local variables, in order to avoid messing up the APPS
-		uint16_t apps1_value = adc1_APPS1;
+		uint16_t apps1_value = adc1_APPS1; // reading sensor values
 		uint16_t apps2_value = adc1_APPS2;
 
 		uint16_t bps1_value = adc1_BPS1;
 		uint16_t bps2_value = adc1_BPS2;
 
 
-		//Perform input validation
+		// SAFETY CHECKS
+
+		// APPS1/APPS2 CHECK (Throttle Position Sensors)
+
+		//Perform input validation and ensures values are within the expected range
 		if(apps1_value < dl_params->min_apps_value){
 			//APPS1 is probably unplugged. Act accordingly
 		}else if(apps1_value > dl_params->max_apps_value){
-			//indicative of APPS1 short
+			//indicative of APPS1 short (APPS1 short circuit detected)
 			//goto DL_end;
 		}
 
@@ -146,11 +153,15 @@ void StartDrivingLoop(void * argument){
 
 
 
+		// BPS1/BPS2 Check (Brake Pressure Sensors)
+		// Ensures brake sensors are not faulty.
+
 		if(bps1_value < dl_params->min_BPS_value){
-
+			// Possible BPS1 disconnection
 		}else if(bps1_value > dl_params->max_BPS_value){
-
+			// Possible BPS1 short circuit
 		}
+
 
 
 		//Repeat these safety checks for BPS 2.
@@ -170,11 +181,13 @@ void StartDrivingLoop(void * argument){
 		 *
 		 * If this happens, we want to set the torque/speed output to zero. This will only reset itself once
 		 * the brakes are set to less than a certain threshold. Honestly evil.
+		 *
+		 * Rachan: IF both accelerator and brake are pressed at the same time, the system registers a fault and cuts motor power.
 		 */
 
 		if((bps1_value > dl_params->bps_plausibility_check_threshold) && (apps1_value > dl_params->apps_plausibility_check_threshold)){
 			//This means that the pedal and brake are checked simultaneously
-			dl_status = Implausible;
+			dl_status = Implausible; // set status to error
 		}
 
 
@@ -185,7 +198,7 @@ void StartDrivingLoop(void * argument){
 
 		//Compute torque/velocity or whatever
 		if(dl_status == Plausible){
-
+			// Compute motor output
 		}
 
 
